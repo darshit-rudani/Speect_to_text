e1cea486d75d95f6c416d6edde8df42d199ae7f9
Darshit Rudani <darshit112001@users.noreply.github.com>
2021-03-24 15:53:40 +0530

    Adding App icon and code divided on different papers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'input.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,86 +17,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SpeechScreen extends StatefulWidget {
-  @override
-  _SpeechScreenState createState() => _SpeechScreenState();
-}
-
-class _SpeechScreenState extends State<SpeechScreen> {
-  stt.SpeechToText _speech;
-  bool _isListening = false;
-  String _textSpeech = 'Press the button to start speaking';
-  List data = [];
-  int count = 0;
-
-  void onListen() async {
-    if (!_isListening) {
-      bool available = await _speech.initialize(
-        onStatus: (val) => print('onStatus: $val'),
-        onError: (val) => print('onError: $val'),
-      );
-      if (available) {
-        setState(
-          () {
-            _isListening = true;
-          },
-        );
-        _speech.listen(
-          onResult: (val) => setState(
-            () {
-              _textSpeech = val.recognizedWords;
-            },
-          ),
-        );
-      }
-    } else {
-      setState(
-        () {
-          _isListening = false;
-          _speech.stop();
-          data.insert(count, _textSpeech);
-          count++;
-          _textSpeech = '';
-        },
-      );
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _speech = stt.SpeechToText();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Speech To Text'),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: AvatarGlow(
-        animate: _isListening,
-        glowColor: Theme.of(context).primaryColor,
-        endRadius: 100,
-        duration: Duration(milliseconds: 2000),
-        repeatPauseDuration: Duration(milliseconds: 100),
-        repeat: true,
-        child: FloatingActionButton(
-          onPressed: () => onListen(),
-          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
-        ),
-      ),
-      body: SingleChildScrollView(
-        reverse: true,
-        child: Container(
-          padding: EdgeInsets.fromLTRB(25, 25, 25, 150),
-          child: Text(
-            _textSpeech,
-            style: TextStyle(fontSize: 32, fontWeight: FontWeight.w900),
-          ),
-        ),
-      ),
-    );
-  }
-}
